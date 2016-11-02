2d8149ff20d25711134f79aab34c8e4e3b1d6cd7
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-11-03 00:51:08 +0100

    Add test and default route

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {Router, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory, IndexRoute} from 'react-router';
 import LandingPage from '../LandingPage';
 
 export default class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={LandingPage}>
+        <Route path="/" >
+          <IndexRoute component={LandingPage}/>
         </Route>
       </Router>
     );
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 ReactDOM.render(
