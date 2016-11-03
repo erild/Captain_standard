22846f72fdd782e7bc22650c42c66eba1c36af5e
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-11-03 21:54:42 +0100

    fix typo and use var

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
- ls
- pwd
  echo "-----BEGIN RSA PRIVATE KEY-----" > $TRAVIS_BUILD_DIR/key
  echo "$SSH_KEY_RAW" |tr " " "\n" >> $TRAVIS_BUILD_DIR/key
  echo "-----END RSA PRIVATE KEY-----" >> $TRAVIS_BUILD_DIR/key
  export SSH_KEY=$TRAVIS_BUILD_DIR/key
- test $TRAVIS_BRANCH = "testprod" && slc deploy -s main http+ssh://cs.ragg.fr:8701 $TRAVIS_BUILD_DIR/../Captain_standard-1.0.0.tgz
+ echo "Deploying..."
+ test $TRAVIS_BRANCH = "testprod" && slc deploy -s main http+ssh://$PRODUCTION_SERVER:8701 $TRAVIS_BUILD_DIR/../Captain_standard-1.0.0.tgz
