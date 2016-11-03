18ba7a34a802e6ac8a202aa0baba3d37fbab00f4
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-11-03 13:37:25 +0100

    fix zsh config and install python

--- ansible/roles/common/files/vim_zsh.sh
+++ ansible/roles/common/files/vim_zsh.sh
@@ -5,17 +5,17 @@ if [[ -z $USER ]]; then
     USER=vagrant
 fi
 cd
-wget http://people.via.ecp.fr/~admin/ganeti/fichiers-config/zlogin
-wget http://people.via.ecp.fr/~admin/ganeti/fichiers-config/zlogout
-wget http://people.via.ecp.fr/~admin/ganeti/fichiers-config/zshrc
-wget http://people.via.ecp.fr/~admin/ganeti/fichiers-config/zshenv
+wget http://formation-debian.via.ecp.fr/fichiers-config/zlogin
+wget http://formation-debian.via.ecp.fr/fichiers-config/zlogout
+wget http://formation-debian.via.ecp.fr/fichiers-config/zshrc
+wget http://formation-debian.via.ecp.fr/fichiers-config/zshenv
 mv zlogin zlogout zshrc zshenv /etc/zsh/
 cp /etc/zsh/zshrc /root/.zshrc
 chown root:root /root/.zshrc
 cp /etc/zsh/zshrc $USERHOME/.zshrc
 chown $USER:$USER $USERHOME/.zshrc
-wget http://people.via.ecp.fr/~admin/ganeti/fichiers-config/dir_colors
+wget http://formation-debian.via.ecp.fr/fichiers-config/dir_colors
 mv dir_colors /etc/
 
-wget http://people.via.ecp.fr/~admin/ganeti/fichiers-config/vimrc
+wget http://formation-debian.via.ecp.fr/fichiers-config/vimrc
 mv vimrc /etc/vim/
