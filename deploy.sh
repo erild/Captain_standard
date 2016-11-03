d7a831fb0ab15b46450acfd2a3f4955a430abfb1
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-11-03 17:32:47 +0100

    test deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
-export SSH_KEY=$PWD/key
-slc --version
-slc deploy -s main http+ssh://cs.ragg.fr:8701 ../Captain_standard-1.0.0.tgz
+ ls
+ pwd
+ echo "-----BEGIN RSA PRIVATE KEY-----" > $TRAVIS_BUILD_DIR/key
+ echo "$SSH_KEY_RAW" |tr " " "\n" >> $TRAVIS_BUILD_DIR/key
+ echo "-----END RSA PRIVATE KEY-----" >> $TRAVIS_BUILD_DIR/key
+ export SSH_KEY=$TRAVIS_BUILD_DIR/key
+ test $TRAVIS_BRANCH = "testprod" && slc deploy -s main http+ssh://cs.ragg.fr:8701 $TRAVIS_BUILD_DIR/../Captain_standard-1.0.0.tgz
