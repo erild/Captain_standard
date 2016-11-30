26faa273c35f4e1158c5db6db8c6ff070d9d36f3
Quentin Vieillard <quentin@myminifactory.com>
2016-11-30 15:37:34 +0100

    Add project model and relation to linters

--- bin/autoupdate.js
+++ bin/autoupdate.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = require(path.resolve(__dirname, '../server/server'));
 const ds = app.datasources.db;
-const tables = ['Customer', 'User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'UserCredential', 'UserIdentity', 'Linter', 'PersistedModel'];
+const tables = ['Customer', 'User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'UserCredential', 'UserIdentity', 'Linter', 'PersistedModel', 'Project', 'ProjectLinter'];
 ds.autoupdate(tables, err => {
   if (err) {
     throw err;
