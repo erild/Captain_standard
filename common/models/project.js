301cfbf1d815b3de24a5bfefee2bfb22f73cb095
Paul dG <pdesgarets@users.noreply.github.com>
2017-01-05 15:42:59 +0100

    FIx style

--- common/models/project.js
+++ common/models/project.js
@@ -150,34 +150,33 @@ module.exports = function (Project) {
     })
   };
 
-  Project.observe('after save', function(ctx, next) {
+  Project.observe('after save', (ctx, next) => {
     if (ctx.instance && ctx.isNewInstance) {
-      let baseUrl = app.get('url').replace(/\/$/, '')
-      let webhookConf = {
+      const baseUrl = app.get('url').replace(/\/$/, '')
+      const webhookConf = {
         'name': 'web',
-        'active':true,
+        'active': true,
         'events': ['pull_request'],
-        'config': {'url':baseUrl+'/api/Projects/linters-exec','content_type': 'json'}
+        'config': {'url': baseUrl + '/api/Projects/linters-exec', 'content_type': 'json'}
       };
-      agent.post('/repos/'+ctx.instance.full_name+'/hooks', webhookConf);
+      agent.post(`/repos/${ctx.instance.full_name}/hooks`, webhookConf);
     }
     next();
   });
 
-  Project.observe('before delete', function(ctx, next) {
-    if(ctx.where.hasOwnProperty('id')){
-      Project.findById(ctx.where.id, (err, result) => {
+  Project.observe('before delete', (ctx, next) => {
+    if (ctx.where.hasOwnProperty('id')) {
+      Project.findById(ctx.where.id, (err, project) => {
         if (!err) {
-          let project = result;
-          if(project) {
-            agent.get('/repos/'+project.full_name+'/hooks').then(res => {
-              let hookUrl = app.get('url').replace(/\/$/, '')+'/api/Projects/linters-exec';
+          if (project) {
+            agent.get(`/repos/${project.full_name}/hooks`).then(res => {
+              const hookUrl = app.get('url').replace(/\/$/, '') + '/api/Projects/linters-exec';
               res.forEach(hook => {
-                if(hook.name =='web' && hook.config.url == hookUrl) {
-                  agent.delete('/repos/'+project.full_name+'/hooks/'+hook.id);
+                if (hook.name === 'web' && hook.config.url === hookUrl) {
+                  agent.delete(`/repos/${project.full_name}/hooks/${hook.id}`);
                 }
-              })
-            })
+              });
+            });
           }
         }
         next();
