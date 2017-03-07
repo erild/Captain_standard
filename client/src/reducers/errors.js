72ae58411ad36ed7a384ce2944c6ae010711d344
Paul des Garets <paul.des-garets@student.ecp.fr>
2017-03-07 15:37:19 +0100

    Clear errors on route change

--- client/src/reducers/errors.js
+++ client/src/reducers/errors.js
@@ -10,6 +10,9 @@ export default (state = [], action) => {
     case 'REMOVE_ERROR':
       newState = state.filter((error, index) => index !== action.payload);
       break;
+    case 'CLEAR_ERRORS':
+      newState = [];
+      break;
     default:
       newState = state;
   }
